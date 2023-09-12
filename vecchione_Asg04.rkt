2bd40f2caa5d93122f701d9fc20d5a60c1482067
Gio (Laptop) <giovannivec21@gmail.com>
2023-09-12 18:40:52 -0500

    Added Asg04

---
+++ vecchione_Asg04.rkt
@@ -0,0 +1,8 @@
+;Programmer: Giovanni Vecchione
+;Date: 9/12/23
+;Asg 04
+;Objective:Write two recursive functions that calculate the mercator series to 100 terms.
+;In the first version, determine the sign of each term with a named subprogram.
+;The second version uses a lambda function instead of the named subprogram.
+;Create a .rkt file that contains the definitions and tests the functions.
+;
