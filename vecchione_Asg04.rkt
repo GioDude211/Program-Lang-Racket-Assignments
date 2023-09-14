5c8deaeed7a58f34d1adf8ffaa8b907b5b67c5de
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-09-14 17:51:16 -0500

    Finished up some functions in Asg04

--- vecchione_Asg04.rkt
+++ vecchione_Asg04.rkt
@@ -11,9 +11,41 @@
 
 #lang racket
 
-(define (mercatorNamed x)
-    ())
 
-(define (mercatorUnNamed y)
-    ())
+; Named recursive function for mercator
+(define (mercator-named n)
+  (define (sign i)
+    (if (even? i) 1 -1))
+  
+  (define (term i)
+    (/ (sign i) (exact->inexact i)))
+  
+  (define (mercator-recursive n i sum)
+    (if (= i n)
+        sum
+        (mercator-recursive n (+ i 1) (+ sum (term i)))))
+  
+  (mercator-recursive n 1 0))
 
+; Un-named recursive function for mercator
+(define (mercator-lambda n)
+  (define (term i)
+    (/ ((if (even? i) (lambda (x) x) (lambda (x) (- x))) i) (exact->inexact i)))
+  
+  (define (mercator-recursive n i sum)
+    (if (= i n)
+        sum
+        (mercator-recursive n (+ i 1) (+ sum (term i)))))
+  
+  (mercator-recursive n 1 0))
+
+(provide mercator-named mercator-lambda)
+
+; Testing functions
+(define (test-mercator function-name)
+  (display (format "Testing ~a...\n" function-name))
+  (define result (function-name 100))
+  (display (format "Mercator(100) = ~a\n" result)))
+
+(test-mercator mercator-named)
+(test-mercator mercator-lambda)
