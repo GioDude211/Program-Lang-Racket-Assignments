5bd0e39a7afeadfa29d90e263b833a4bfa0ca5b4
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-09-14 00:22:28 -0500

    Began def functions in asg04

--- vecchione_Asg04.rkt
+++ vecchione_Asg04.rkt
@@ -1,8 +1,19 @@
 ;Programmer: Giovanni Vecchione
 ;Date: 9/12/23
 ;Asg 04
+;
 ;Objective:Write two recursive functions that calculate the mercator series to 100 terms.
 ;In the first version, determine the sign of each term with a named subprogram.
 ;The second version uses a lambda function instead of the named subprogram.
 ;Create a .rkt file that contains the definitions and tests the functions.
 ;
+;x - x2/2 + x3/3 - x4/4 ... = ln(x+1)
+
+#lang racket
+
+(define (mercatorNamed x)
+    ())
+
+(define (mercatorUnNamed y)
+    ())
+
