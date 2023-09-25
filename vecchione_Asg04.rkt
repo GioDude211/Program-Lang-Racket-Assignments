b231c7fa9de5cadcfcfecab79031276b84566fa7
Gio (Laptop) <giovannivec21@gmail.com>
2023-09-24 22:58:41 -0500

    Updated

--- vecchione_Asg04.rkt
+++ vecchione_Asg04.rkt
@@ -1,6 +1,6 @@
 ;Programmer: Giovanni Vecchione
-;Date: 9/12/23
-;Asg 04
+;Date: 9/24/23
+;Asg 04 - CORRECTED/RESUBMIT
 ;
 ;Objective:Write two recursive functions that calculate the mercator series to 100 terms.
 ;In the first version, determine the sign of each term with a named subprogram.
@@ -11,31 +11,33 @@
 
 #lang racket
 ;CHECK
+;i is the terms and x is the input
 
-; Named recursive function for mercator
+; Named subprogram recursive function for mercator
 (define (findSign i)
   (if (= (remainder i 2) 0)
       -1.0
       1.0))
 
-; Un-named recursive function for mercator
+
 (define (mercator i x)
   (if (< i 2)
       x
       (+ (/ (* (findSign i) (expt x i)) i)
          (mercator (- i 1) x))))
 
-(define (mercator-unamed i x)
+; lambda subprogram recursive function for mercator
+(define (mercator-lambda i x)
   (if (< i 2)
       x
       (+ (/ (* ((lambda (i) (if (= (remainder i 2) 0) -1.0 1.0)) i)
               (expt x i))
          i)
-         (mercator (- i 1) x))))
+         (mercator-lambda (- i 1) x))))
 
 (mercator 100 0.5) 
 
 (mercator 100 -0.5) 
 
-(mercator-unamed 100 0.5)
-(mercator-unamed 100 -0.5)
+(mercator-lambda 100 0.5)
+(mercator-lambda 100 -0.5)
