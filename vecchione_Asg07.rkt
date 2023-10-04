3d7a76aa84068e8e32f942261c27a93618c2e03e
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-10-04 00:34:22 -0500

    Works and seems done

--- vecchione_Asg07.rkt
+++ vecchione_Asg07.rkt
@@ -5,3 +5,23 @@
 ;Use the method described in class.
 
 #lang racket
+
+;Function: myBadSort
+;Example of using a poor sorting algorithm with recursion
+
+(define (myBadSort lst)
+  (define (findMax list)
+    (if (empty? (rest list))
+        (first list)
+        (max (first list) (findMax (rest list)))))
+  (define (myRemove item list)
+    (cond [(empty? list) '()]
+          [(= item (first list)) (rest list)]
+          [else (cons (first list) (myRemove item (rest list)))]))
+  (if (empty? lst)
+      '()
+      (let ((max-val (findMax lst)))
+        (append (myBadSort (myRemove max-val lst)) (list max-val)))))
+
+;; Example usage:
+(myBadSort '(20 13 74 5 12 9 22 95 22 6 101 72 3 53 33 21 96))
