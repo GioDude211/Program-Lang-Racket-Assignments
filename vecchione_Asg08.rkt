908b2b4dcab24666362a6584f1499e3bd8bde401
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-10-10 18:23:01 -0500

    asg 08 init

---
+++ vecchione_Asg08.rkt
@@ -0,0 +1,9 @@
+; Programmer: Giovanni Vecchione
+; Date: 10/10/23
+; Subject: Asg 08
+;Write a recursive function that given a number and a sorted binary tree, returns a sorted tree with the number insertd.
+;Write a recursive function that given a list of numbers and a tree, returns a tree with all of the numbers inserted.
+;Write a recursive function that given a sorted tree, returns a list of numers in ascending order.
+;Details in the slides.
+
+#lang racket
