cc775cde9abedd3656dd3e8c157e1c12d70736e5
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-10-12 17:39:35 -0500

    Finished the first function, still work on it

--- vecchione_Asg08.rkt
+++ vecchione_Asg08.rkt
@@ -7,3 +7,60 @@
 ;Details in the slides.
 
 #lang racket
+
+; Define a node structure
+(struct node (val left right))
+
+; Recursive function to insert a key into a BST
+(define (insert root key)
+  (cond
+    [(null? root) 
+     (node key #f #f)]
+    [(< key (node-val root)) 
+     (node (node-val root)
+           (if (node-left root)
+               (insert (node-left root) key)
+               (node key #f #f))
+           (node-right root))]
+    [else
+     (node (node-val root)
+           (node-left root)
+           (if (node-right root)
+               (insert (node-right root) key)
+               (node key #f #f)))]))
+
+; Function to perform an inorder traversal of a BST
+(define (inorder root)
+  (if (not root)
+      '()
+      (append (inorder (node-left root)) (list (node-val root)) (inorder (node-right root)))))
+
+; Function to print the BST using inorder traversal
+(define (print-tree root)
+  (display "Tree: ")
+  (display (inorder root))
+  (newline))
+
+; Function to insert a key and print the BST before and after insertion
+(define (insert-and-print-tree root key)
+  (display "Before Inserting: ")
+  (print-tree root)
+  (let ([new-root (insert root key)])
+    (display "After Inserting ")
+    (display key)
+    (display ": ")
+    (print-tree new-root)
+    new-root))
+
+; Example usage:
+; Construct a basic tree for testing
+;       20
+;      /  \
+;    15    25
+;   / \
+;  10  18
+(define root (node 20 (node 15 (node 10 #f #f) (node 18 #f #f)) (node 25 #f #f)))
+
+; Insert a new node and print tree before and after insertion
+(define new-root (insert-and-print-tree root 17))
+
