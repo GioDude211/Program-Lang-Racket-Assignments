49d0e82f09a8d274d13089c76afe340c116aad04
Gio (Laptop) <giovannivec21@gmail.com>
2023-10-23 17:48:41 -0500

    added asg09

---
+++ vecchione_Asg09.rkt
@@ -0,0 +1,7 @@
+;Programmer: Giovanni Vecchione
+;Date: 10/22/23
+;Subject; Asg 09
+;Write a function that translates, scales, rotates, and draws polygons.
+;See 'COSC 3301 Coding 9.pptx' for details.
+
+#lang racket
