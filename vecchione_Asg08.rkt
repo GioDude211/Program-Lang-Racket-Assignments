68663e9f8a233f0ec33a9754aa48215dce87d603
Gio (Laptop) <giovannivec21@gmail.com>
2023-11-04 19:14:33 -0500

    fixed asg08

--- vecchione_Asg08.rkt
+++ vecchione_Asg08.rkt
@@ -9,63 +9,83 @@
 #lang racket
 
 ; FUNCTION 1 - DONE
-;Side Note - Had some severe issues keeping track of the syntax, however after learning a better visual
-;I was able to format it similar to functions I've worked with in other languages.
-
 (define (tree-insert val tree)
+  
   (cond
-    [(null? tree) (list val)] ; Return the value as a list if tree is empty
-    
-    [(equal? (cdr tree) '()) ; if only a root node, create subnodes
-      (cond
-        [(<= val (car tree)) (list (car tree) (list val) '())]  ;#1 If value is less than or equal to, go left 
-        [else (list (car tree) '() (list val))])]               ;#1 else go right
-      
-    [(<= val (car tree))                               ;#2 If the number is less than or equal to the root, recurse on the left subtree
-      (list (car tree) (tree-insert val (cadr tree))
-        (if (pair? (cddr tree)) (caddr tree) '()))]
 
-      [else (list (car tree)                          ;#2 else recurse on the right
-                (if (pair? (cdr tree))
-                    (cadr tree) '())
-                (tree-insert val (if (pair? (cddr tree)) (caddr tree) '())))]
+    ; if the tree is empty, return a new list with the number
+    [(null? tree) (list val)]
 
-  )
-)
+    ; check if the number is greater than the root (car tree)
+    [(> val (car tree))
+     
+     ; is the right subtree empty?
+     (if (equal? (cdr tree) '())
+
+         ; yes, right subtree is empty
+         (list (car tree) (list '() (tree-insert val '())))
+
+         ; checking if the left subtree of the right subtree is empty,  
+         (if (equal? (cadr tree) '())
+
+             ; if it is -
+             (list (car tree) (list (cadr tree) (tree-insert val (caddr tree))))
+
+             ; if not empty
+             (list (car tree) (list (caadr tree) (tree-insert val (cadadr tree))))))]
+
+    ; the number is less than the root
+    [else
+
+     ; check if root is only element in tree
+     (if (equal? (cdr tree) '())
+
+         ; if it is.. return list with root
+         (list (car tree) (list (tree-insert val '()) '()))
+
+         ; checks if root node contains subtrees
+         (if (equal? (cadr tree) '())
+             ; root node doesn't contain subtrees
+             (list (car tree) (list (tree-insert val '()) (caddr tree)))
+
+             ; root node does have subtrees
+             (list (car tree) (list (tree-insert val (caadr tree)) (cadr (cadr tree))))))]))
+
+
 
 ; FUNCTION 2 - DONE
-;SideNote - Not exact to the output however, output matches list positioning
-; Expected output: '(22 ((7 ((7 ((4 ((3) (5))) ())) (16 ((8 ((8) ())) (17))))) (25 (() (34 ((32) (67))))))
 (define (list-to-tree lst1 tree2)
     (if (null? lst1) tree2
       (list-to-tree (cdr lst1) (tree-insert (car lst1) tree2)))
 )
 
 
-;FUNCTION 3 - Done
-(define (tree-to-list tree3)
+
+;Function 3 - DONE
+(define (tree-to-list tree)
   (cond
-    [(null? tree3) '()] ; checks if empty, otherwise returns empty tree
+    [(null? tree) '()] ; Tree is empty, return an empty list
+    [(null? (cdr tree)) (list (car tree))] ; Tree is just a root node, return list with root value
+    [else ; Tree has subtrees
+     (append 
+       (tree-to-list (caadr tree)) ; Process the left subtree
+       (list (car tree)) ; Include the root
+       (tree-to-list (cadadr tree)))])) ; Process the right subtree
 
-    [(equal? (cdr tree3) '()) (list (car tree3))];if only root, return first element
 
-    [else (append (tree-to-list (cadr tree3)) (list (car tree3)) (tree-to-list (caddr tree3)))]
-    ; ^ else add the tree-to-list of cadr tree to the first node, also add to the tree-to-list of caddr of tree
-  )
-)
 
 
 
 ; Testing Function 1
-(display (tree-insert 8 '())) ; should display '(8)
+(display (tree-insert 8 '()))
 (newline)
-(display (tree-insert 12 '(8))) ; should display '(8 () (12))
+(display (tree-insert 12 '(8)))
 (newline)
-(display (tree-insert 3 '(8))) ; should display '(8 (3))
+(display (tree-insert 3 '(8)))
 (newline)
-(display (tree-insert 12 '(8 (3)))) ; should display '(8 (3) (12))
+(display (tree-insert  12 '(8 ((3) ()))))
 (newline)
-(display (tree-insert 4 '(8 (3) (12)))) ; should display '(8 (3 (4)) (12))
+(display (tree-insert 4 '(8 ((3) (12)))))
 (newline)
 
 ; Testing Function 2
