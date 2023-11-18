019ad9f6b53a87f91eb0d09365572a54d1901aed
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-11-18 11:33:48 -0600

    updates

--- testfile_asg14.2_vecchione.rkt
+++ testfile_asg14.2_vecchione.rkt
@@ -44,9 +44,9 @@
 ;drawToScreen FUNCTION -NOTHING WRONG HERE
 (define (drawToScreen dc myPolygon)
   (let ([xTrans 800]
-        [yTrans 1280]
-        [xScale .15]
-        [yScale .15])
+        [yTrans 1100]
+        [xScale .2]
+        [yScale .2])
     ; Convert the polygon to screen coordinates
     (send myPolygon scale xScale yScale)
     (send myPolygon translate xTrans yTrans)
@@ -59,7 +59,6 @@
     ; Convert the polygon back to world coordinates
     (send myPolygon translate (- xTrans) (- yTrans))
     (send myPolygon scale (/ 1.0 xScale) (/ 1.0 yScale))))
-
 
 ;Function creates the image (MAIN)
 (define (create-fractal-image depth rotateAmount x1 y1 scaleFactorX scaleFactorY LengthX LengthY)
@@ -74,19 +73,17 @@
     ;Take into account the rotation and translation
     (define x2 (- x1 (* scaledDistanceX (cos rotateAmount))))
     (define y2 (- y1 (* scaledDistanceY (sin rotateAmount))))
-    
-    ; create polygon - FIX THIS
-    (define myPolygon (new dc-path%)) 
-    (send myPolygon move-to x1 y1) ; input points (works like x-axis and y-axis)
-    (send myPolygon line-to x2 y1)
-    (send myPolygon line-to x2 y2)
-    (send myPolygon line-to x1 y2)
-    (send myPolygon close)
+
+
+     ; ; create a new line for the poly
+    (define poly (new dc-path%))
+    (send poly move-to x1 y1)
+    (send poly line-to x2 y2)
     
     ; draw polygon
-    (drawToScreen my-dc myPolygon)                 
+    (drawToScreen my-dc poly)                 
     (set! numPoly (+ 1 numPoly))
-
+   
     
     ;Do-Loop determines how many new branches
     (do ([i 0 (+ i 1)])
@@ -95,11 +92,11 @@
       ;adjust factors for length
       (let* ([scaleFactorY (cond [(= i 0) .5]    
                                   [(= i 1) .7]   
-                                  [(= i 2) .7])]
+                                  [(= i 2) 0.7])]
              
              [scaleFactorX (cond [(= i 0) 1]    
                                   [(= i 1) 0.8]   
-                                  [(= i 2) 0.4])]  
+                                  [(= i 2) .4])]  
              ;rotational change (angle)
               [rotateFactor (cond [(= i 0) -1.3]     
                                   [(= i 1) .5]     
@@ -113,7 +110,7 @@
 
 
 
-(create-fractal-image depth rotateAmount startX1 startY1 scaleFactorX scaleFactorY baseLengthX baseLengthY) ; CALL CREATE_FRACTAL_IMAGE FUNCTION
+(create-fractal-image depth rotateAmount  startX1 startY1 scaleFactorX scaleFactorY baseLengthX baseLengthY) ; CALL CREATE_FRACTAL_IMAGE FUNCTION
 
 (display "Number of polygons drawn: ")
 (display numPoly)
@@ -125,6 +122,6 @@ my-bitmap
 ;track the transformations to keep track of the new x and y coordinates. For some reason I believe this is where the issue resides. I don't think its because of
 ;the duplicate poly function
 
-;ISSUE 2: Okay i was able to figure out the form of the tree, next step is to work on the x and y coordinates still
+;ISSUE 2: Theres a problem with the rotation of the branches
 
 ;IT IS CLOSE BUT IT IS NOT TRANSLATING TO THE CORRECT LOCATIONS
